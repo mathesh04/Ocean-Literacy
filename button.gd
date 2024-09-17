b257964d87c41fefcd1288ebd79c2e679cb46523
mathesh04 <mmathesh838@gmail.com>
2024-09-17 12:39:10 +0530

    initail commit of godot project

---
+++ button.gd
@@ -0,0 +1,7 @@
+extends Button
+
+
+
+
+func _on_pressed() -> void:
+	get_tree().change_scene_to_file("res://rolesel.tscn") # Replace with function body.
---
+++ node_2d.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://fisher_man.tscn")#Replace with function body.
+
+
+func _on_button_6_pressed() -> void:
+	get_tree().change_scene_to_file("res://tourist.tscn") # Replace with function body.
+
+
+func _on_button_5_pressed() -> void:
+	get_tree().change_scene_to_file("res://ship_captain.tscn")# Replace with function body.
+
+
+func _on_button_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://marine_engineer.tscn") # Replace with function body.
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://salvor.tscn") # Replace with function body.
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://coastal_gaurd.tscn") # Replace with function body.
